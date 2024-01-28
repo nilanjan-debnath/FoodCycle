078a8ee42fe38f294163a7b21e19df837ce5021a
Rivu Basu <rivu.basu13042@gmail.com>
2024-01-29 00:29:41 +0530

    CSS payment

--- food_cycle/src/components/Payment.jsx
+++ food_cycle/src/components/Payment.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./css/Payment.module.css";
 
 function Payments() {
-    return (
-        <>
-        <h3>This is the payment page</h3>
-        </>
-    )
+  return (
+    <>
+      <h3 className={styles.heading}>This is the payment page</h3>
+    </>
+  );
 }
 export default Payments;
