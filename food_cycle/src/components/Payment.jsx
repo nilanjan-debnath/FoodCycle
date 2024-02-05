237841ab64b397c16be2ba2511f718b1e69c1cca
Rivu Basu <rivu.basu13042@gmail.com>
2024-02-06 00:01:51 +0530

    Payment page(not completed till now)

--- food_cycle/src/components/Payment.jsx
+++ food_cycle/src/components/Payment.jsx
@@ -4,7 +4,73 @@ import styles from "./css/Payment.module.css";
 function Payments() {
   return (
     <>
-      <h3 className={styles.heading}>This is the payment page</h3>
+      <center>
+        <h3>This is the payment page</h3>
+      </center>
+      <div
+        className={`card ${styles.pay_container}`}
+        style={{ width: "50rem" }}
+      >
+        <div class="card-body">
+          <form>
+            <div class="mb-3">
+              <label for="amount" class="form-label">
+                Enter amount
+              </label>
+              <input type="text" class="form-control" id="amount" />
+            </div>
+            <div class="mb-3">
+              <label for="paymentType" class="form-label">
+                How would you like to pay?
+              </label>
+              <input type="text" class="form-control" id="paymentType" />
+            </div>
+
+            <div>Fill up your personal information</div>
+
+            <div className={styles.personal}>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  className={`form-control ${styles.inpFirst}`}
+                  id="firstName"
+                  placeholder="First Name"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  className={`form-control ${styles.inpLast}`}
+                  id="lastName"
+                  placeholder="Last Name"
+                />
+              </div>
+            </div>
+            <div class="mb-3">
+              <input
+                type="email"
+                className={`form-control ${styles.inp_email}`}
+                id="email"
+                placeholder="Email Address"
+              />
+            </div>
+            <div class="mb-3">
+              <input
+                type="text"
+                className={`form-control ${styles.inp_cardno}`}
+                id="cardNumber"
+                placeholder="Card Number"
+              />
+            </div>
+
+            <center>
+              <button type="submit" class="btn btn-primary">
+                Submit
+              </button>
+            </center>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
