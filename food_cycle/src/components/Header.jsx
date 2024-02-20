e91b671bbeda2522ddf379355dd71cd411339484
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-02-21 02:20:09 +0530

    home part1

--- food_cycle/src/components/Header.jsx
+++ food_cycle/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 import Activities from './Activities';
@@ -8,6 +8,7 @@ import FeedBack from './Feedback';
 import Progress from './Progress';
 import AdminDashBoard from './AdminDash';
 import Home from "./Home";
+import Logo from "../../public/images/logo.png";
 
 function Header() {
     const [nav, setNav] = useState(false);
@@ -17,22 +18,16 @@ function Header() {
 
     return (
         <>
-            <nav style={nav === true ? {top: "0%"}:{top: "-12%"}}>
+            <nav style={nav === true ? { top: "0%" } : { top: "-16%" }}>
+                <div className="name">
+                    <div className="image">
+                        <img src={Logo} alt="" />
+                    </div>
+                    <h1 id="name">FoodCycle</h1>
+                </div>
                 <ul>
                     <li>
                         <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/adminDas" >AdminDash</Link>
-                    </li>
-                    <li>
-                        <Link to="/login" >Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/feedBack" >Feedback</Link>
-                    </li>
-                    <li>
-                        <Link to="/payment" >Payment</Link>
                     </li>
                     <li>
                         <Link to="/progress" >Progress</Link>
@@ -40,9 +35,21 @@ function Header() {
                     <li>
                         <Link to="/activities" >Activities</Link>
                     </li>
+                    <li id="payment">
+                        <Link to="/payment" >Raise your helping hand</Link>
+                    </li>
+                    <li id="login">
+                        <Link to="/login" >Login</Link>
+                    </li>
+                    {/* <li>
+                        <Link to="/adminDas" >AdminDash</Link>
+                    </li> */}
+                    {/* <li>
+                        <Link to="/feedBack" >Feedback</Link>
+                    </li> */}
                 </ul>
-                <button id="menu" style={nav === true ? {opacity: "0"}:{opacity: "1"}} onClick={handleNav}><i className="fa-solid fa-bars"></i></button>
-                <button id="cross" style={nav === true ? {opacity: "1"}:{opacity: "0"}} onClick={handleNav}><i class="fa-solid fa-xmark"></i></button>
+                <button id="menu" style={nav === true ? { opacity: "0" } : { opacity: "1" }} onClick={handleNav}><i className="fa-solid fa-bars"></i></button>
+                <button id="cross" style={nav === true ? { opacity: "1" } : { opacity: "0" }} onClick={handleNav}><i class="fa-solid fa-xmark" style={{color: "white", opacity: "0.5"}}></i></button>
             </nav>
 
             <Routes>
--- food_cycle/src/components/Home.jsx
+++ food_cycle/src/components/Home.jsx
@@ -1,10 +1,63 @@
 import React from "react";
+import styles from "./css/home.module.css";
+import FoodImage from "../../public/images/food.png";
 
 function Home() {
     return (
-        <>
-        <h3>this is the home page</h3>
-        </>
+        <div className={`${styles.main}`}>
+            <div className={`${styles.box1}`}>
+                <div className={`${styles.one}`}></div>
+                <div className={`${styles.two}`}></div>
+                <div className={`${styles.three}`}></div>
+                <div className={`${styles.four}`}></div>
+                <div className={`${styles.left}`}>
+                    <h1>Food Donation</h1>
+                    <p>Welcome to our platform dedicated to food donation, where every act of generosity makes a meaningful difference in someone's life. </p>
+                    <p>At <span style={{ color: "blue" }}>FoodCycle</span>, we believe in the power of collaboration to address food insecurity in our communities. Restaurants, caterers, and individuals hosting events like weddings or birthdays can partner with us to donate surplus food and help those in need. Restaurants and event hosts have the opportunity to donate excess food that might otherwise go to waste. They can choose to offer it for free or at a discounted rate, according to their preferences. By partnering with us, they not only reduce food waste but also contribute to a meaningful cause.</p>
+                    <p>By participating in our food donation initiative, restaurants and event hosts can make a tangible impact on the lives of individuals and families facing hunger. Together, we can transform surplus food into nourishment and hope for those in need.</p>
+                </div>
+                <div className={`${styles.right}`}>
+                    <img src={FoodImage} alt="" />
+                </div>
+            </div>
+            <div className={`${styles.box2}`}>
+                <div className={`${styles.row}`}>
+                    <div className={`${styles.left}`}>
+                        <h1>Join Our Community</h1>
+                        <p>Be a part of our mission to alleviate hunger and reduce food waste. Join our vibrant community of restaurants, event hosts, and individuals dedicated to making a positive impact through food donation.</p>
+                        <p>Joining our community means becoming a catalyst for change. By partnering with us, you contribute to a more sustainable and compassionate world where no one goes hungry.</p>
+                        <p>As a member, you'll have the opportunity to donate surplus food from your restaurant or event. You can choose to offer it for free or at a discounted rate, empowering you to give back in a way that aligns with your values.</p>
+                        <p>Your contributions matter. By joining our community, you help ensure that excess food finds its way to those who need it most. Together, we can turn surplus into sustenance and make a real difference in the lives of individuals and families facing hunger.</p>
+                        <p>Ready to be a part of something meaningful? Join our community today and help us make a difference, one meal at a time. Visit our app to learn more about how you can get involved and start making a positive impact in your community.</p>
+                    </div>
+                    <div className={`${styles.right}`}>
+                        <img src="https://www.shutterstock.com/shutterstock/photos/2377762257/display_1500/stock-vector-join-our-community-vector-illustration-for-referral-program-2377762257.jpg" alt="" />
+                    </div>
+                </div>
+                <div className={`${styles.row}`}>
+                    <div className={`${styles.right}`}>
+                        <img src="https://www.shutterstock.com/shutterstock/photos/2377762257/display_1500/stock-vector-join-our-community-vector-illustration-for-referral-program-2377762257.jpg" alt="" />
+                    </div>
+                    <div className={`${styles.left}`}>
+                        <h1>Help Your Self and Help Others</h1>
+                        <p>At [Your Project Name], we believe in the power of mutual support and collective action. Our mission is simple: to create a platform where you can help yourself while making a positive impact on the lives of others.</p>
+                        <p>When you contribute to our project, you're not just helping others; you're also helping yourself. By participating in our initiatives, whether through food donations, volunteering, or advocacy, you become part of something greater than yourself. You experience the fulfillment that comes from knowing you've made a difference in someone else's life.</p>
+                        <p>At [Your Project Name], we believe in creating win-win situations. By participating in our project, you not only help those in need but also enrich your own life in the process. Whether you're a business looking to donate surplus food or an individual seeking to lend a helping hand, together, we can make a difference.</p>
+                    </div>
+                </div>
+                <div className={`${styles.row}`}>
+                    <div className={`${styles.left}`}>
+                        <h1>We are for Everyone</h1>
+                        <p>At [Your Project Name], inclusivity is at the heart of everything we do. We believe that everyone has the power to make a difference, regardless of background, identity, or circumstance. Our project is built on the principle that when we come together as a diverse and inclusive community, we can create positive change that benefits everyone.</p>
+                        <p>Our doors are open to all who share our commitment to making the world a better place. Whether you're a business owner, a community leader, a student, or someone who simply cares about making a difference, you are welcome here. Together, we can harness the power of our diversity to tackle some of the most pressing challenges facing our communities.</p>
+                        <p>No matter who you are or where you come from, there's a place for you in our community. Join us in our mission to create a more just, equitable, and compassionate world for all. Together, let's build a future where everyone has the opportunity to thrive.</p>
+                    </div>
+                    <div className={`${styles.right}`}>
+                        <img src="https://www.shutterstock.com/shutterstock/photos/2377762257/display_1500/stock-vector-join-our-community-vector-illustration-for-referral-program-2377762257.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Home;
